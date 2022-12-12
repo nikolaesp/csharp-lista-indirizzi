c550e8e60a935e92fdd77cdc4d2ac91e0b1c0751
Nikola Krstevski <nikolakrstevskids@gmail.com>
2022-12-12 14:52:59 +0100

    Add project files.

---
+++ Addresses.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Addresses
+    {
+        private string Name;
+        private string Surname;
+        private string Street;
+        private string City;
+        private string Province;
+        private string ZIP;
+
+        public Addresses(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.Name = name;
+            this.Surname = surname; 
+            this.Street= street;    
+            this.City= city;
+            this.Province = province;
+            this.ZIP = zip;
+        }
+        public string GetZip() { return this.ZIP; }
+        public void SetZip(string zip) { this.ZIP = zip;}
+        public string GetSurname()
+        {
+            return GetSurname();
+        }
+
+        public void SetSurname(string value)
+        {
+            SetSurname(value);
+        }
+
+        public string GetStreet()
+        {
+            return GetStreet();
+        }
+
+        public void SetStreet(string value)
+        {
+            SetStreet(value);
+        }
+
+        public string GetCity()
+        {
+            return GetCity();
+        }
+
+        public void SetCity(string value)
+        {
+            SetCity(value);
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string value)
+        {
+            Name = value;
+        }
+
+        public override string ToString()
+        {
+            return "Person: " + Name + " " + Surname + "\t Address: "+ Street+ "\t"+City+"\t"+Province+"\t"+ZIP;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharp_lista_indirizzi;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+List<Addresses> listaddress = new List<Addresses>();
+
+try
+{
+    StreamReader file = File.OpenText("D:\\ProgGeneration\\csharp-lista-indirizzi\\File\\addresses.csv");
+
+    while (!file.EndOfStream)
+    {
+        string line = file.ReadLine();
+
+       
+        Console.WriteLine(line);
+        string[] addresses = line.Split(',');
+
+        string nome = addresses[0];
+        string cognome = addresses[1];
+        string via = addresses[2];
+        string cita = addresses[3];
+        string provincia = addresses[4];
+        string zip = addresses[5];
+
+
+       Addresses indirizi = new Addresses(nome,cognome,via,cita,provincia,zip);
+        listaddress.Add(indirizi);
+    }
+
+    file.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+Console.WriteLine("-----------------------------------------------EOF---!---*");
+foreach (Addresses indirizzo in listaddress)
+{
+    Console.WriteLine(indirizzo);
+}
